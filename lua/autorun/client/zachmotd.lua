f94fe6551dd16d4727fbf36daf5c1e89ddbfe784
Zach Cheatham <zachcheatham@gmail.com>
2018-01-12 13:22:04 -0600

    Update url

--- lua/autorun/client/zachmotd.lua
+++ lua/autorun/client/zachmotd.lua
@@ -17,31 +17,31 @@ function ulx.showMotdMenu(steamid)
 	end
 
 	local html = vgui.Create("DHTML", container)
-	html:OpenURL("http://untitledttt.site.nfoservers.com/motd/")
+	html:OpenURL("http://etherealisles.com/motd/")
 	html:SetSize(ScrW() * 0.8, ScrH() * 0.8)
 	html:SetPos(ScrW() / 2 - html:GetWide() / 2, ScrH() / 2 - html:GetTall() / 2)
 	html:MakePopup()
-	
+
 	html.startTime = SysTime()
-	
-	html.Paint = function(self, w, h)	
+
+	html.Paint = function(self, w, h)
 		draw.RoundedBox(0, 0, 0, w, h, Color(255, 255, 255))
-		
+
 		surface.DisableClipping(true)
-		
+
 		-- Draw a shadow (fun)
 		for i=1, 3 do
 			draw.RoundedBox(3 + i, -i, i - 1, w + i * 2, h + i -1, Color(0, 0, 0, 25))
 		end
-		
+
 		surface.DisableClipping(false)
 	end
-	
+
 	html:AddFunction("game", "closewindow", function()
 		container:Remove()
 		hook.Call("MOTDClosed")
 	end)
-	
+
 	local closeButton = vgui.Create("DButton", container)
 	closeButton:SetFont("MOTDCloseButton")
 	closeButton:SetText("CLOSE")
@@ -52,7 +52,7 @@ function ulx.showMotdMenu(steamid)
 	end
 	closeButton.UpdateColours = function(skin)
 	end
-	
+
 	closeButton.DoClick = function()
 		container:Remove()
 		hook.Call("MOTDClosed")
